c7b7f0612aa1d9bfee7b387dca80e5fae2a4f676
Mohd Azeem <153131338+azeemsln@users.noreply.github.com>
2024-08-02 02:44:12 +0530

    Time: 6 ms (33.22%), Space: 44.7 MB (5.42%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str= String.valueOf(x);
+        for(int i=0;i<=str.length()/2;i++){
+            if(str.charAt(i)==str.charAt(str.length()-1-i)){
+                continue;
+            }
+            else
+                return false;
+        }
+        return true;
+        
+    }
+}
